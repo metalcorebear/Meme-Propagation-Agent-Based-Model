a4fb6d187ee8579737f69e2b2216985629ce84c5
metalcorebear <43913302+metalcorebear@users.noreply.github.com>
2020-03-25 12:43:01 -0400

    Update model_params.py

--- model_params.py
+++ model_params.py
@@ -7,7 +7,7 @@ Created on Wed Mar 25 11:54:25 2020
 
 # density = network edge density.
 # network_size = number of nodes in network.
-# neg_bias = propability of node having a negative valence on an idealized, 1D political spectrum.
+# neg_bias = probability of node having a negative valence on an idealized, 1D political spectrum.
 # meme_density = initial seed density of nodes that have shared the meme.
 
 parameters = {'density':0.85, 'network_size':100, 'neg_bias':0.5, 'meme_density':0.01}
