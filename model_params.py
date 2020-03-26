40bb116394d58c0a7faa5f29b1b06f3a1515558c
metalcorebear <43913302+metalcorebear@users.noreply.github.com>
2020-03-26 10:37:37 -0400

    Updated params
    Added steps parameter.

--- model_params.py
+++ model_params.py
@@ -9,5 +9,6 @@ Created on Wed Mar 25 11:54:25 2020
 # network_size = number of nodes in network.
 # neg_bias = probability of node having a negative valence on an idealized, 1D political spectrum.
 # meme_density = initial seed density of nodes that have shared the meme.
+# steps = number of steps to run the model.
 
-parameters = {'density':0.85, 'network_size':100, 'neg_bias':0.5, 'meme_density':0.01}
+parameters = {'density':0.85, 'network_size':100, 'neg_bias':0.5, 'meme_density':0.01, 'steps':1000}
